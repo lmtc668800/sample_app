a1dd0168e50d7375f4b660253d3eea13fa789525
Summer <lmtc668800@gmail.com>
2014-04-04 19:38:52 +0900

    modified production.rb again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,7 @@ SampleApp::Application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :sass
+  config.assets.precompile += ['custom.css.scss']
 ##
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
